f2427b32e64c0f9de983074e374093ce9b78b18a
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-10-11 13:03:59 -0300

    rolled back suggestions, kept original implementation

--- project-toast/src/components/ToastShelf/ToastShelf.js
+++ project-toast/src/components/ToastShelf/ToastShelf.js
@@ -9,12 +9,14 @@ function ToastShelf() {
   const { toastList, dismissToast } = useToastContext();
   // Adding the escape hook is better since it will live as long as toasts exists
 
-  const handleDismiss = React.useCallback(dismissToast, []);
   // dismissToast callback is regenerated on every render if it is passes as a dependency array in escape key hook
   // useCallback fixes this, but is an extra step that can be avoided if we just pass an empty array for the hook's useEffect dependencies
-  // will depend on the implementation
 
-  const effectDismissToast = useEscapeKey(handleDismiss);
+  // const handleDismiss = React.useCallback(dismissToast, []);
+
+  // will depend on the implementation, I choose to have the warnings only in the hooks since it has more direct relation
+  // to the logic I want to achieve (avoid regenerating the callback on every rerender)
+  const effectDismissToast = useEscapeKey(dismissToast);
 
   return (
     toastList.length > 0 && (
--- project-toast/src/hooks/index.js
+++ project-toast/src/hooks/index.js
@@ -9,6 +9,6 @@ export const useEscapeKey = (callback) => {
     }
     window.addEventListener("keydown", handleEscape);
     return () => window.removeEventListener("keydown", handleEscape);
-  }, [callback]);
+  }, []);
   return callback;
 };
