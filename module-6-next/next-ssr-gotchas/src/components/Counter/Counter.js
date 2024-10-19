b9263e534de7248bb200515b3c96c7fe012a2ad0
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-10-19 18:31:26 -0300

    spinner solution added

--- module-6-next/next-ssr-gotchas/src/components/Counter/Counter.js
+++ module-6-next/next-ssr-gotchas/src/components/Counter/Counter.js
@@ -1,14 +1,40 @@
 "use client";
 import React from "react";
+import Spinner from "../Spinner/Spinner";
 
 // Golden rule: The first render should produce the same markup on the server AND the client (mind side effects: localstorage values used )
 // for rendering) - Hydration mismatches
 
 // Effects do not run on the server, regardless if they are client components (the effect only runs on the client)
 // Effects run AFTER hydration takes place
+// function Counter() {
+//   const [count, setCount] = React.useState(0);
+//   // will run on the very first render and read the values stored in the client (if any)
+//   React.useEffect(() => {
+//     const local = Number(window.localStorage.getItem("saved-count"));
+//     if (local === null) {
+//       return;
+//     }
+//     setCount(local);
+//   }, []);
+
+//   React.useEffect(() => {
+//     window.localStorage.setItem("saved-count", count);
+//   }, [count]);
+
+//   return (
+//     <button className="count-btn" onClick={() => setCount(count + 1)}>
+//       Count: {count}
+//     </button>
+//   );
+// }
+
+// export default Counter;
+
+// This is a more honest solution (it does not pick to show the "probable" initial value - 0 - but instead shows a loading state)
 function Counter() {
-  const [count, setCount] = React.useState(0);
-  // will run on the very first render and read the values stored in the client (if any)
+  const [count, setCount] = React.useState(null);
+
   React.useEffect(() => {
     const local = Number(window.localStorage.getItem("saved-count"));
     if (local === null) {
@@ -18,13 +44,14 @@ function Counter() {
   }, []);
 
   React.useEffect(() => {
-    window.localStorage.setItem("saved-count", local);
-  }, [local]);
+    window.localStorage.setItem("saved-count", count);
+  }, [count]);
 
-  return;
-  <button className="count-btn" onClick={() => setCount(count + 1)}>
-    Count: {count}
-  </button>;
+  return (
+    <button className="count-btn" onClick={() => setCount(count + 1)}>
+      Count: {typeof count === "number" ? count : <Spinner />}
+    </button>
+  );
 }
 
 export default Counter;
